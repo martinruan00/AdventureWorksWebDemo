57579c0638a510b7c2cde340b59eeb31f1c1aa1e
The Quyet Nguyen <nguyenthequyet33@gmail.com>
2020-11-06 22:52:02 +0100

    init Angular client

--- AdventureWorksWebDemo/ClientApp/src/app/app.module.ts
+++ AdventureWorksWebDemo/ClientApp/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { HumanresourcesComponent } from './humanresources/humanresources.component';
+import { RouterModule } from '@angular/router';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    HumanresourcesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    RouterModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
